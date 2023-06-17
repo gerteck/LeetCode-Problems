3ae9e0a8fee9641434b3a804f8c883d0fc7f5a2c
gerteck <changerteck@gmail.com>
2023-06-17 16:14:44 +0800

    Problem 217

--- 217_Contains_Duplicate.py
+++ 217_Contains_Duplicate.py
@@ -1,1 +1,13 @@
-print("Hello World")
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # nums it the list / Array of numbers
+        # sort the array then check 
+        sorted_nums = nums.sort()
+        prev_num = sorted_nums[0]
+        sorted_nums.pop(0)
+        for num in sorted_nums:
+            if (num == prev_num):
+                return True
+            prev_num = num
+
+        return False
